2ffc0fc68106bd2658388f636db8626a3ed8d070
simyeji <skeosk424@gmail.com>
2022-08-17 16:18:18 +0900

    저장

--- src/main/java/com/mybatis/bankMembers/BankMembersController.java
+++ src/main/java/com/mybatis/bankMembers/BankMembersController.java
@@ -1,8 +1,12 @@
 package com.mybatis.bankMembers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BankMembersController {
+	
+	@Autowired
+	private BankMembersService service;
 
 }
--- src/main/java/com/mybatis/bankMembers/BankMembersDAO.java
+++ src/main/java/com/mybatis/bankMembers/BankMembersDAO.java
@@ -1,5 +1,14 @@
 package com.mybatis.bankMembers;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BankMembersDAO {
+	
+	@Autowired
+	private SqlSession session;
+	private final String NAMESPACE = "com.mybatis.bankMembers.BankMembersDAO.";
 
 }
